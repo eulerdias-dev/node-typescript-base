f619696caf64d652483dd5f7b615815661619076
Euler Dias <51454031+eulerdias-dev@users.noreply.github.com>
2019-07-02 18:34:40 -0300

    Inserção do mongoose e controller de checarAPI

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
 import routes from './routes'
 
 class App {
@@ -8,11 +9,24 @@ class App {
     public constructor () {
       this.express = express()
       this.middlewares()
+      this.database()
+      this.addRoutes()
     }
 
     private middlewares (): void {
       this.express.use(express.json())
       this.express.use(cors())
-      this.express.routes(routes)
+    }
+
+    private database (): void {
+      mongoose.connect('mongodb://localhost:27017/db', {
+        useNewUrlParser: true
+      })
+    }
+
+    private addRoutes (): void {
+      this.express.use('/api', routes)
     }
 }
+
+export default new App().express
